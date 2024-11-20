d7c26f6f4f68fbbc65fd3684ac4739e2af2394f6
Quang Ngo <46818455+quangngonz@users.noreply.github.com>
2024-11-20 13:25:46 +0700

    add ViteExpress integration and restructure server setup

--- server.js
+++ server.js
@@ -17,8 +17,4 @@ app.use('/stocks', stocksRoutes);
 app.use('/teacher', teacherRoutes);
 app.use('/user', userRoutes);
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-  console.log(`http://localhost:${PORT}`);
-});
+module.exports = app;
