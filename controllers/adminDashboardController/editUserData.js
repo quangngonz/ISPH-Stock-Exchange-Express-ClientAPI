efc76ad9eb7758354730dc6c0b9ee14284467370
Quang Ngo <46818455+quangngonz@users.noreply.github.com>
2025-01-31 21:53:14 +0700

    Updates user data editing logic to merge current data with new data before saving

--- controllers/adminDashboardController/editUserData.js
+++ controllers/adminDashboardController/editUserData.js
@@ -1,5 +1,5 @@
 const { database } = require('../../services/firebaseService');
-const { ref, set } = require('firebase/database');
+const { ref, set, get } = require('firebase/database');
 
 const editUserData = async (req, res) => {
   const { userId, userData } = req.body;
@@ -9,9 +9,13 @@ const editUserData = async (req, res) => {
   }
 
   const userRef = ref(database, `users/${userId}`);
+  const currentData = (await get(userRef)).val();
+
+  // merge current data with new data
+  const updatedData = { ...currentData, ...userData };
 
   try {
-    await set(userRef, userData);
+    await set(userRef, updatedData);
     res.send('User data updated successfully.');
   } catch (err) {
     console.error(err);
