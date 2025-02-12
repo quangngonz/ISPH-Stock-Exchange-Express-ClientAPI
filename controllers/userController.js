8f95ae026518fd040378a6b6dd2a44603df4a115
Quang Ngo <46818455+quangngonz@users.noreply.github.com>
2025-02-12 14:08:56 +0700

    fix auth

--- controllers/userController.js
+++ controllers/userController.js
@@ -110,7 +110,8 @@ const checkIfUserExists = async (req, res) => {
 const createUser = async (req, res) => {
   const { userId, house, username } = req.body;
 
-  const userRecord = admin.auth(userId);
+  // Get the user record from Firebase Auth
+  const userRecord = await admin.auth().getUser(userId);
 
   const userData = {
     user_id: userId,
